750ad9c06db34f003eb388e58dba918693d7fff7
nstitley <46876281+nstitley@users.noreply.github.com>
2019-12-04 01:30:09 -0600

    All links are working correctly. Check-boxes are working for both lists (job and skills). The queries are setup correctly as well now. The correct jobs are showing up after the applicant selects his/her skills. PDF file will not open in acrobat reader.

--- src/java/edu/jsu/mcis/cs425/project2/Database.java
+++ src/java/edu/jsu/mcis/cs425/project2/Database.java
@@ -1,16 +1,13 @@
 package edu.jsu.mcis.cs425.project2;
 
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.HashMap;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
-import javax.servlet.ServletException;
 import javax.sql.DataSource;
 
 public class Database {
@@ -85,40 +82,38 @@ public class Database {
             
             Connection conn = getConnection();
             
-            try (PreparedStatement statement = conn.prepareStatement(query)) {
+            PreparedStatement statement = conn.prepareStatement(query);
                 
-                statement.setString(1, Integer.toString(userid));
-                boolean hasResults = statement.execute();
+            statement.setString(1, Integer.toString(userid));
+            boolean hasResults = statement.execute();
                 
-                try (ResultSet resultset = statement.getResultSet()) {
+            ResultSet resultset = statement.getResultSet();
                     
-                    while (resultset.next()) {
+            while (resultset.next()) {
                         
-                        String description = resultset.getString("description");
-                        int id = resultset.getInt("id");
-                        int user = resultset.getInt("userid");
+                String description = resultset.getString("description");
+                int id = resultset.getInt("id");
+                int user = resultset.getInt("userid");
                         
-                        result.append("<input type=\"checkbox\" name=\"skills\" value=\"");
-                        result.append(id);
-                        result.append("\" id=\"skills_").append(id).append("\" ");
+                result.append("<input type=\"checkbox\" name=\"skills\" value=\"");
+                result.append(id);
+                result.append("\" id=\"skills_").append(id).append("\" ");
                         
-                        if (user != 0) {
+                if (user != 0) {
                             
-                            result.append("checked");
+                    result.append("checked");
                             
-                        }
-                        
-                        result.append(">");
-                        result.append("<label for=\"skills_").append(id).append("\">").append(description).append("</label><br /><br />");
-                        
-                    }
-                    
-                    resultset.close();
-                    statement.close();
                 }
+                        
+                result.append(">");
+                result.append("<label for=\"skills_").append(id).append("\">").append(description).append("</label><br /><br />");
+                        
             }
+                    
+            resultset.close();
+            statement.close();
         }
-        
+   
         catch (Exception SQLException) {}
         
         skill = result.toString();
@@ -137,17 +132,17 @@ public class Database {
             Connection conn = getConnection();
             PreparedStatement statement = conn.prepareStatement(delQuery);
             statement.setString(1, Integer.toString(userid));
-            
-                statement = conn.prepareStatement(insQuery);
+            int alter = statement.executeUpdate();
+            statement = conn.prepareStatement(insQuery);
                 
-                for (int i = 0; i < skills.length; i++) {
+            for (int i = 0; i < skills.length; i++) {
                     
-                    statement = conn.prepareStatement(insQuery);
-                    statement.setString(1, Integer.toString(userid));
-                    statement.setString(2, skills[i]);
-                    statement.execute();           
+                statement = conn.prepareStatement(insQuery);
+                statement.setString(1, Integer.toString(userid));
+                statement.setString(2, skills[i]);
+                statement.execute();           
                     
-                }
+            }
         }
         
         catch (Exception SQLException) {}
@@ -156,7 +151,7 @@ public class Database {
     String getJobsListAsHTML(int userid, String[] skills) {
         
         String query = "SELECT jobs.id, jobs.name, a.userid FROM jobs LEFT JOIN\n" +
-        "(SELECT * FROM applicants_to_jobs WHERE userid= ?) AS a ON jobs.id = a.jobsid\n" +
+        "(SELECT * FROM applicants_to_jobs WHERE userid = ?) AS a ON jobs.id = a.jobsid\n" +
         "WHERE jobs.id IN (SELECT jobsid AS id FROM (applicants_to_skills JOIN skills_to_jobs\n" +
         "ON applicants_to_skills.skillsid = skills_to_jobs.skillsid) WHERE applicants_to_skills.userid = ?) ORDER BY jobs.name;";
         
@@ -203,13 +198,14 @@ public class Database {
     void setJobsList(int userid, String[] jobs) {
         
         String delQuery = "DELETE FROM cs425_p2.applicants_to_jobs WHERE userid = ?;";
-        String insQuery = "INSERT INTO cs425_p2.applicants_to_jobs (userid, jobsid)\n VALUES (?, ?);";
+        String insQuery = "INSERT INTO cs425_p2.applicants_to_jobs (userid, jobsid) VALUES (?, ?);";
         
         try {
             
             Connection conn = getConnection();
             PreparedStatement statement = conn.prepareStatement(delQuery);
-            statement.setString(1, Integer.toString(userid));            
+            statement.setString(1, Integer.toString(userid));
+            int alter = statement.executeUpdate();
             
             statement = conn.prepareStatement(insQuery);
             
